a41f1a397a5419f38cc94a77961ce21e40db336d
p-suryavanshi <pavans6350@gmail.com>
2026-03-12 11:22:19 +0530

    Added reduce function

--- ClassWork/Tasks/764-Pavan-Suryavanshi/cw-w4d2-4.js
+++ ClassWork/Tasks/764-Pavan-Suryavanshi/cw-w4d2-4.js
@@ -17,3 +17,31 @@ console.log("Concatenated String is: " , concat);
 //Find the maximum in the numbers arrayy..
 const maximum = numbers.reduce( (acc, num) => acc > num ? acc : num);
 console.log("Maximum numbers in Array is: " , maximum);
+
+//
+const transctions = [
+    {type: "deposit", amount: 1000},
+    {type: "Withdraw", amount: 300},
+    {type: "deposit", amount: 700},
+    {type: "Withdraw", amount: 100},
+    {type: "deposit", amount: 2000},
+    {type: "Withdraw", amount: 500},
+];
+
+const deposits = transctions.filter( (transction) => transction.type === "deposit" )
+const total = deposits.reduce( (acc , d ) => acc + d.amount , 0 );
+console.log("Total deposits: " + total);
+
+const withdrawls = transctions.filter( (t) => t.type === "Withdraw" )
+                        .reduce( (sum , t) => sum + t.amount, 0 );
+console.log("Total Withdrawls: ",withdrawls);
+
+const net = total - withdrawls;
+console.log("Net Balance: " , net);
+
+const transactionCount = transctions.reduce( (acc, t) => {
+    acc[t.type] = (acc[t.type] || 0 ) + 1;
+    return acc;
+}, {} );
+
+console.log("Transction Counts: ", transactionCount);
